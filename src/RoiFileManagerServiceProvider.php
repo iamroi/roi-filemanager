15dddbf21dd7ed4920d7b70083092772a40e3d14
Raja Munisamy <mrajasays@gmail.com>
2020-05-09 20:43:47 +1000

    Add service provider

---
+++ src/RoiFileManagerServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Iamroi\RoiFileManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class RoiFileManagerServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        # code...
+    }
+
+    public function register()
+    {
+        # code...
+    }
+}
